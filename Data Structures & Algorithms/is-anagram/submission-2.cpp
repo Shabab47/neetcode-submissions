1dfa4aebadcd3e8d33f495d6c998be01a0725029
Shabab <shababnandigram28@gmail.com>
2026-07-24 20:13:43 +0600

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        for(int i=0;i<max(s.size(),t.size());i++)if(s[i]!=t[i])return 0;
+        return 1;
+    }
+};
