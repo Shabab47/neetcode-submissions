7f9208bb7b9bd0b976d7abf25e825728a116cf9e
Shabab <shababnandigram28@gmail.com>
2026-07-24 20:15:05 +0600

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        set<int> s;
+        for(int i=0;i<nums.size();i++)s.insert(nums[i]);
+        if(s.size()==nums.size())return 0;
+        else return 1;
+    }
+};
