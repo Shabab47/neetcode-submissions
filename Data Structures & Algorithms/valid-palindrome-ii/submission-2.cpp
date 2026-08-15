3f00a60fe811f1c26eeaf655a1221b1ed55c24bc
Shabab <shababnandigram28@gmail.com>
2026-08-15 23:42:01 +0600

    Add: valid-palindrome-ii - submission-2

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-2.cpp
@@ -0,0 +1,24 @@
+class Solution {
+private:
+    bool p(string& s,int l,int r){
+        while(l<r){
+            if(s[l]!=s[r])return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+public:
+    bool validPalindrome(string s){
+        int l=0;
+        int r=s.size()-1;
+        while (l<r){
+            if (s[l]!=s[r]){
+                return p(s,l+1,r)||p(s,l,r-1);
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+};
